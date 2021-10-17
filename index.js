16c887fdbfdcf0b3ac1d641284363b6d54a89c87
Matt Weichel <maweiche@gmail.com>
2021-10-17 16:27:05 -0700

    added employee prompt

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+//link to HTML generator file
 const generaterHTML = require('./generateHTML');
-
 //team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern');
+//node 
+const fs = require('fs');
+const inquirer = require('inquirer');
+//team set
+const team = [];
 
-//node modules
-
-//team
+//start of team prompt
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Choose employee's role.",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else{
+                    console.log("Please enter a name.")
+                    return false;
+                }
+            }
+        }
+    ])
+}
 
