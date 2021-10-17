827c7ec083149d94c141268678c705ccd8bbf9e5
Matt Weichel <maweiche@gmail.com>
2021-10-17 11:22:02 -0700

    employee test complete

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,8 @@
-const { TestWatcher } = require('@jest/core');
 const Employee = require('../lib/Employee');
 
 //creates employee object
-TestWatcher('creates an employee object', () => {
+describe("Employee Object")
+it('creates an employee object from name, id, email', () => {
     const employee = new Employee('Matt', 30, 'maweiche@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
@@ -10,3 +10,26 @@ TestWatcher('creates an employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
+//get id
+describe("Employee ID")
+it('captures Employee ID input', () => {
+    const employee = new Employee('Matt', 30, 'maweiche@gmail.com');
+
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
+
+//get email
+describe("Employee email")
+it('captures Employee email input', () => {
+    const employee = new Employee('Matt', 30, 'maweiche@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+//get role
+describe("Role of Employee")
+it('captures role of Employee input', () => {
+    const employee = new Employee('Matt', 30, 'maweiche@gmail.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+});
