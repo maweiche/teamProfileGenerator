628c805170520ae31d07cf03939543cbcecca160
Matt Weichel <maweiche@gmail.com>
2021-10-18 08:45:43 -0700

    linked style sheet to output, colors need to be fixed

--- generateHTML.js
+++ generateHTML.js
@@ -104,7 +104,7 @@ const generatePage = function (employeeCards) {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/style.css"/>
+        <link rel="stylesheet" href="./style.css"/>
     </head>
 
     <body>
